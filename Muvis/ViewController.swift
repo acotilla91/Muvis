47fff241a0428e58b2c54d6fc4c6b90950516c90
Alejandro Cotilla <acotilla91@gmail.com>
2020-09-05 13:46:22 -0400

    Create "Single App" template.

---
+++ Muvis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Muvis
+//
+//  Created by Alejandro Cotilla on 9/5/20.
+//  Copyright © 2020 Carolco LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
