5b717a440faf01b51379b69fcca7defacfd731d1
Alejandro Cotilla <acotilla91@gmail.com>
2020-09-06 19:35:55 -0400

    Test API requests from main view controller.

--- Muvis/ViewController.swift
+++ Muvis/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import NVActivityIndicatorView
 
 class ViewController: UIViewController {
+    
+    private var activityIndicator: NVActivityIndicatorView!
+    
+    private var mediaCategories: [MediaCategory] = []
+    
+    // MARK: - UI Setup -
+
+    func setupActivityIndicator() {
+        let indicatorSize: CGFloat = 60.0
+        let indicatorOrigin = CGPoint(x: UIScreen.main.bounds.width/2 - indicatorSize/2, y: UIScreen.main.bounds.height/2 - indicatorSize/2)
+        let indicatorFrame = CGRect(x: indicatorOrigin.x, y: indicatorOrigin.y, width: indicatorSize, height: indicatorSize)
+        activityIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .circleStrokeSpin, color: UIColor(hex: "#434446"), padding: 8.0)
+        view.addSubview(activityIndicator)
+    }
+
+    // MARK: - View Cycle -
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Hide "Back" label
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        
+        setupActivityIndicator()
+        loadContent()
     }
 
+    // MARK: - API Communication -
 
+    private func loadContent() {
+        activityIndicator.startAnimating()
+        
+        TMDbAPIService.shared.requestMediaCategories { (categories) in
+            self.mediaCategories = categories
+
+            // TODO: Load collection view with requested categories
+            
+            self.activityIndicator.stopAnimating()
+        }
+    }
 }
 
