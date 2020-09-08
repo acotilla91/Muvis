b90c5fbdd7552fb6559cb187292ad07a0a906a50
Alejandro Cotilla <acotilla91@gmail.com>
2020-09-07 20:21:18 -0400

    Force light theme.

--- Muvis/SceneDelegate.swift
+++ Muvis/SceneDelegate.swift
@@ -12,6 +12,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        // Force light theme
+        if #available(iOS 13.0, *) {
+            window?.overrideUserInterfaceStyle = .light
+        }
+
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 }
